7970b2caff834bb0c1a25e5685b493643e2d0e57
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-07-22 16:50:27 +0100

    feat: updated cv file

--- src/styles/Logo.js
+++ src/styles/Logo.js
@@ -8,17 +8,17 @@ const Logo = styled.img`
     top: 12vh;
     left: 1.5vw;
     z-index: 990;
-    animation: float 10s ease-in-out infinite;
+    animation: float 15s ease-in-out infinite;
     @media (min-width: 800px) {
         display: revert;  
     }
 
     @keyframes float {
-        0% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(50px)}
-	    25% {transform: rotateX(-10deg) rotateY(-10deg) translateX(-20px) translateY(10px)}
+        0% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
+	    25% {transform: rotateX(-10deg) rotateY(-10deg) translateX(-20px) translateY(50px)}
         50% {transform: rotate(-20deg) rotateX(-10deg) rotateY(-10deg) translateX(-50px) translateY(30px)}
         75% {transform: rotateX(-10deg) rotateY(10deg) translateX(10px) translateY(50px) perspective(9cm)}
-        100% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(50px)}
+        100% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
     }
 `;
 
@@ -29,7 +29,7 @@ const LogoName = styled.h1`
     left: -5px;
     top: -3vh;
     z-index: 20;
-    animation: glitch 4.5s linear infinite reverse, glitch 0.5s linear infinite;
+    animation: glitch 5s linear infinite reverse, glitch 0.2s linear infinite;
 
     @keyframes glitch {
         from {perspective(6cm); opacity: 0.8}
