593bc26fcc5132851a28d2c2150a5ee26b97faf1
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-08-06 02:05:24 +0100

    feat: glitch animation on Logo

--- src/styles/Logo.js
+++ src/styles/Logo.js
@@ -1,4 +1,49 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const float = keyframes`
+    0% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
+    25% {transform: rotateX(-10deg) rotateY(-10deg) translateX(-20px) translateY(50px)}
+    50% {transform: rotate(-20deg) rotateX(-10deg) rotateY(-10deg) translateX(-50px) translateY(30px)}
+    75% {transform: rotateX(-10deg) rotateY(10deg) translateX(10px) translateY(50px) perspective(9cm)}
+    100% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
+`;
+
+const glitch = keyframes`
+    0% {text-shadow: none}
+    50% {
+        text-shadow:
+            0 -14px 2px rgba(100, 100, 100, 0.55), 
+            0 -12px 0.5px rgba(80, 80, 80, 0.5),
+            0 -10px 0px rgba(86, 86, 86, 0.45),
+            0 -8px 0px rgba(52, 52, 52, 0.5),
+            0 -6px 0px rgba(52, 52, 52, 0.55),
+            0 2px 0px rgba(52, 52, 52, 0.3),
+            0 3px 0px rgba(52, 52, 52, 0.2)
+        ;
+    }
+    100% { text-shadow: none}
+`;
+
+const glitchOnPC = keyframes`
+    0% {text-shadow: none}
+    25% {
+        text-shadow: 
+            0 -10px 0px rgba(86, 86, 86, 0.45),
+            0 -8px 0px rgba(52, 52, 52, 0.5),
+            0 -6px 0px rgba(52, 52, 52, 0.55),
+        ;
+    }
+    75% {
+        text-shadow: 
+            0 -14px 2px rgba(100, 100, 100, 0.55), 
+            0 -12px 0.5px rgba(80, 80, 80, 0.5),
+            0 -10px 0px rgba(86, 86, 86, 0.45),
+            0 -8px 0px rgba(52, 52, 52, 0.4),
+            0 -6px 0px rgba(52, 52, 52, 0.35)
+        ;
+    }
+    100% {text-shadow: none}
+`;
 
 const Logo = styled.img`
     display: none;
@@ -7,17 +52,9 @@ const Logo = styled.img`
     position: absolute;
     left: 35%;
     z-index: 990;
-    animation: float 15s ease-in-out infinite;
+    animation: ${float} 15s ease-in-out infinite;
     @media (min-width: 800px) {
         display: revert;  
-    }
-
-    @keyframes float {
-        0% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
-	    25% {transform: rotateX(-10deg) rotateY(-10deg) translateX(-20px) translateY(50px)}
-        50% {transform: rotate(-20deg) rotateX(-10deg) rotateY(-10deg) translateX(-50px) translateY(30px)}
-        75% {transform: rotateX(-10deg) rotateY(10deg) translateX(10px) translateY(50px) perspective(9cm)}
-        100% {transform: rotate(-10deg) rotateY(10deg) rotateX(-20deg) translateX(-30px) translateY(30px)}
     }
 `;
 
@@ -26,19 +63,20 @@ const LogoName = styled.h1`
     font-weight: 300;
     letter-spacing: 1.5px;
     position: relative;
-    top: 3vh;
+    top: calc(3vh + 8px);
     left: -3px;
     z-index: 20;
-    animation: 5s linear 0s infinite reverse none running glitch, 0.25s linear 0s infinite normal none running glitch;
-
-    @keyframes glitch {
-        from {perspective(6cm); opacity: 0.85}
-        to {perspective(0); opacity: 0.4}
-    }
+    color: ${window.innerWidth > 800 ? 'rgba(0, 0, 0, 0)' : 'rgba(0, 0, 0, 0.1)'};
+    opacity: 0.9;
+    text-shadow: 
+        0.05em 0 0 rgba(255, 0, 0, 0.75),
+        -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),
+        0.025em 0.05em 0 rgba(0, 0, 255, 0.75)
+    ;
+    animation: ${window.innerWidth > 800 ? css`10s ${glitchOnPC} ease-in-out` : css`5s ${glitch} ease`} 0s infinite none running;
 
     @media (min-width: 800px) {
-        top: 7vh;
-        text-shadow: 0px 4px 2px #b2a98f, 0px 8px 10px rgba(0, 0, 0, 0.5);
+        top: calc(7vh + 8px);
     }
 `;
 
